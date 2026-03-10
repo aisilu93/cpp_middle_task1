6a3c2ea449fb42361c18b24e389416167ad9d863
Aisilu Mavletdinova <aisilu93@yandex.ru>
2026-03-10 18:39:50 +0000

    CryptoGuardCtx::Impl implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,10 @@
 #include "../include/cmd_options.h"
 #include "crypto_guard_ctx.h"
 #include <algorithm>
-#include <array>
 #include <iostream>
-#include <openssl/evp.h>
 #include <print>
 #include <stdexcept>
 #include <string>
-
-struct AesCipherParams {
-    static const size_t KEY_SIZE = 32;             // AES-256 key size
-    static const size_t IV_SIZE = 16;              // AES block size (IV length)
-    const EVP_CIPHER *cipher = EVP_aes_256_cbc();  // Cipher algorithm
-
-    int encrypt;                              // 1 for encryption, 0 for decryption
-    std::array<unsigned char, KEY_SIZE> key;  // Encryption key
-    std::array<unsigned char, IV_SIZE> iv;    // Initialization vector
-};
-
-AesCipherParams CreateChiperParamsFromPassword(std::string_view password) {
-    AesCipherParams params;
-    constexpr std::array<unsigned char, 8> salt = {'1', '2', '3', '4', '5', '6', '7', '8'};
-
-    int result = EVP_BytesToKey(params.cipher, EVP_sha256(), salt.data(),
-                                reinterpret_cast<const unsigned char *>(password.data()), password.size(), 1,
-                                params.key.data(), params.iv.data());
-
-    if (result == 0) {
-        throw std::runtime_error{"Failed to create a key from password"};
-    }
-
-    return params;
-}
 
 int main(int argc, char *argv[]) {
     CryptoGuard::ProgramOptions config;
@@ -42,8 +15,6 @@ int main(int argc, char *argv[]) {
         //
         std::string input = "01234567890123456789";
         std::string output;
-
-        OpenSSL_add_all_algorithms();
 
         auto params = CreateChiperParamsFromPassword("12341234");
         params.encrypt = 1;
@@ -77,7 +48,7 @@ int main(int argc, char *argv[]) {
         }
         EVP_CIPHER_CTX_free(ctx);
         std::print("String encoded successfully. Result: '{}'\n\n", output);
-        EVP_cleanup();
+        
         //
         // Конец примера
         //
